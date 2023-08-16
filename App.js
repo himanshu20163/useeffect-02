04cbcb719cb3a50434d58f31623b5a7772abd9c5
unknown <sahu.himanshu2016@gmail.com>
2023-08-16 18:37:25 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Passwordgenrate from './Components/React-state-003/Passwordgenrate';
+import PasswordGenerator from './Components/React-state-003/Passwordgenrate';
+// import Markdown from './Components/React-state-002/Markdown';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Toggle /> */}
+      {/* <Markdown /> */}
+      <PasswordGenerator />
+    </div>
+  );
+}
+
+export default App;
