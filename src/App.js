b4656ccaa04c235b9cdd92428f03efba11f2226d
Himanshu Sahu <109578416+himanshu20163@users.noreply.github.com>
2023-08-21 21:47:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Foodrecipe from './Components/React-useeffect-001/Foodrecipe';
 import Movieapp from './Components/useeffect-02/Movieapp';
 Foodrecipe
 
